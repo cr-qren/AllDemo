0f11c2d50ea74916fbf7676a618277e75b8a6536
Quan Ren 任泉 <35069247+cr-qren@users.noreply.github.com>
2018-07-13 10:27:17 +0800

    Delete update-symbols.sh

--- v2_nest/v2_nest_demo/.exvim.workspace/update-symbols.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.workspace"
-export TOOLS="/home/pi/.vim/tools/"
-export TMP="${DEST}/_symbols"
-export TARGET="${DEST}/symbols"
-sh ${TOOLS}/shell/bash/update-symbols.sh
