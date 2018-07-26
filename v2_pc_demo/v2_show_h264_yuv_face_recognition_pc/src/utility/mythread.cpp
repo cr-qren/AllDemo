79a89d206f31429c805cd8d1b64c72d258026af3
cr-qren <quan.ren@corerain.com>
2018-07-26 09:49:42 +0800

    20180726

--- v2_pc_demo/v2_show_h264_yuv_face_recognition_pc/src/utility/mythread.cpp
+++ v2_pc_demo/v2_show_h264_yuv_face_recognition_pc/src/utility/mythread.cpp
@@ -4,12 +4,13 @@ void *mythread_1(void *arg)
 {
   VideoStream *vs = (VideoStream *)arg;
 
-  vs->client = new Cr_tcp("192.168.2.3", 1234, CLIENT);
+  vs->client = new Cr_tcp("192.168.2.3", 8002, CLIENT);
   vs->start_Reco = true;
 
   FaceSDK::CrFaceSDK *sdk = Init();
   int x = 0;
   int y = 0;
+  int count = 0;
   while(1)
   {
     YUV_DATA yuvData;
@@ -22,15 +23,15 @@ void *mythread_1(void *arg)
     printf("H: %d\n", yuvData.yuvPram->Height);
     printf("C: %d\n", yuvData.yuvPram->Channel);
     printf("SIZE: %d\n", yuvData.yuvPram->BuffSize);
-    printf("buff %s\n", yuvData.yuvBuff);
     /*        yuv -> Mat        */
 
     cv::Mat xx = YUV420Img2RGB_Mat(yuvData.yuvBuff, yuvData.yuvPram->Height, yuvData.yuvPram->Width);
-    char buffy[256];
-    sprintf(buffy, "yuv_%d_%d.jpg",x,y);
-    imwrite(buffy, xx);
-
-    cv::Mat cur_frame = cv::imread("../img_dataset/faces2save_1.jpg");
+    //        char buffy[256];
+    //sprintf(buffy, "yuv_%d_%d.jpg",x,y);
+    //imwrite(buffy, xx);
+    //
+    //    cv::Mat cur_frame = cv::imread("../img_dataset/faces2save_1.jpg");
+    cv::Mat cur_frame = xx;
     FaceSDK::Faces faces2save;
     faces2save.faceCount.totalCount = 1;
     faces2save.faceCount.currentCount = 1;
@@ -44,7 +45,13 @@ void *mythread_1(void *arg)
     for(unordered_map<int, string>::iterator it = id2name.begin(); it!=id2name.end(); it++)
     {
       cout<<"id :" << it->first <<" name : "<< it->second <<endl;
-
+      if(it->second.compare("None")==0)
+      {
+        char buffy[256];
+        sprintf(buffy, "%05d.jpg",count);
+        imwrite(buffy, xx);
+        count ++;
+      }
       /*      show info    <mat, string>     */
 
       if(y == 5)
@@ -57,10 +64,10 @@ void *mythread_1(void *arg)
         x = 0;
       }
       char buffxx[256];
-      sprintf(buffxx, "%d_%d.jpg",x,y);
+      sprintf(buffxx, "%d_%d_%d.jpg",x,y,count);
       vs->mutex_1.lock();
       imageMosaic(vs->resultImg, cur_frame, it->second, x, y, vs->resultImg->width()/5, vs->resultImg->height()/5); 
-      vs->resultImg->save(buffxx, "JPG");
+      //vs->resultImg->save(buffxx, "JPG");
       vs->mutex_1.unlock();
 
       y ++;
--- v2_pc_demo/v2_show_h264_yuv_face_recognition_pc/test/video.cpp
+++ v2_pc_demo/v2_show_h264_yuv_face_recognition_pc/test/video.cpp
@@ -9,7 +9,9 @@ int main(int argc, char *argv[])
 
   VideoStream video;
   video.Init();
-  video.setUrl("rtsp://admin:CPFEGI@192.168.1.168:554/h264/ch1/main/av_stream");
+  //video.setUrl("rtsp://admin:CPFEGI@192.168.1.168:554/h264/ch1/main/av_stream");
+  video.setUrl("rtsp://admin:CPFEGI@192.168.1.168:554/h264/ch2/av_stream");
+  //video.setUrl("rtsp://admin:admin@192.168.1.168:554/1");
   //video.setUrl("rtsp://admin:admin@192.168.1.168:554/");
   video.show();
 
